61b3bc18910c8c7b6a94ba2079ae1e9ac844b696
= <=>
2023-04-18 14:43:36 +0400

    refactored latest games and mini games

--- src/client/src/pages/homeMenu/HomeMenu.jsx
+++ src/client/src/pages/homeMenu/HomeMenu.jsx
@@ -1,13 +1,79 @@
 import React, { useEffect, useState } from "react";
 
 import style from "./homeMenu.module.css";
-import Games from "./games/Games";
-import LatestGame from "./games/latestGame/LatestGame";
+import LatestGame from "./components/latestGame/LatestGame";
+import MiniGames from "./components/miniGames/MiniGames";
 
 export const HomeMenu = () => {
+  const [menuMode, setMenuMode] = useState("latest-game");
+
+  const [menuClasses, setMenuClasses] = useState({
+    "mini-games": "",
+    "latest-game": "selected-li",
+    "art-games": " ",
+  });
+  const [menuItemClases, setMenuItemClasses] = useState({
+    "mini-games": "",
+    "latest-game": "show-item",
+    "art-games": " ",
+  });
+
+  const changeMode = (mode) => {
+    setMenuMode(mode);
+    setMenuClasses({
+      ["mini-games"]: mode === "mini-games" ? "selected-li" : "",
+      ["latest-game"]: mode === "latest-game" ? "selected-li" : "",
+      ["art-games"]: mode === "art-games" ? "selected-li" : "",
+    });
+    setMenuItemClasses({
+      ["mini-games"]: mode === "mini-games" ? "show-item" : "",
+      ["latest-game"]: mode === "latest-game" ? "show-item" : "",
+      ["art-games"]: mode === "art-games" ? "show-item" : "",
+    });
+  };
+
   return (
     <div className={style.homeMenu}>
-      <LatestGame />
+      <ul className={style.header}>
+        <li
+          onClick={() => {
+            changeMode("mini-games");
+          }}
+          className={style["menu-li"] + " " + style[menuClasses["mini-games"]]}
+        >
+          Mini Games
+        </li>
+        <li
+          onClick={() => {
+            changeMode("latest-game");
+          }}
+          className={style["menu-li"] + " " + style[menuClasses["latest-game"]]}
+        >
+          Latest Game
+        </li>
+        <li
+          onClick={() => {
+            changeMode("art-games");
+          }}
+          className={style["menu-li"] + " " + style[menuClasses["art-games"]]}
+        >
+          Art Game
+        </li>
+      </ul>
+      <div
+        className={
+          style["menu-item"] + " " + style[menuItemClases["latest-game"]]
+        }
+      >
+        <LatestGame />
+      </div>
+      <div
+        className={
+          style["menu-item"] + " " + style[menuItemClases["mini-games"]]
+        }
+      >
+        <MiniGames />
+      </div>
     </div>
   );
 };
---
+++ src/client/src/pages/homeMenu/components/miniGames/components/gameContainer.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+import { AiFillLike } from "react-icons/ai";
+import { FaCommentAlt } from "react-icons/fa";
+
+import style from "./gameContainer.module.css";
+
+const GameContainer = () => {
+  return (
+    <div className={style.gameContainer}>
+      <div
+        className={
+          style.gameBackgroundImage + " " + style["miniGameBackgroundImage-1"]
+        }
+      ></div>
+      <p className={style.name}> Batumisken </p>
+      <p className={style.shortDescription}>
+        asiudhasiud asiudhasd asiudhasd asiud aisudhqiwudqwd iquwdh iudwd
+        wudhqiud qiwudhwiud diwuw wuwd w wdwd dwd wdiuqwdh qiwudhqiwudhqiwduhqd
+        qdwuhd iudhwiqudhqd iwuw wudhwd iuwhdiuwhdiqwuh qiwudhqiwud qiduqwiduhd
+        diuwhdw wiudh diuwhdwqdqwuidhd q,dwoasidad
+      </p>
+      <div className={style.indicators}>
+        <ul>
+          <li className={style.likeIcon}>
+            <AiFillLike /> <li className={style.number}> 0 </li>
+          </li>
+
+          <li className={style.deslikeIcon}>
+            <li className={style.number}> 0 </li>
+            <AiFillLike />
+          </li>
+
+          <li className={style.commentIcon}>
+            <FaCommentAlt />
+          </li>
+        </ul>
+        <button className={style.openButton}> Open </button>
+      </div>
+    </div>
+  );
+};
+
+export default GameContainer;
